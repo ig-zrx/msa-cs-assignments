6d72852ba042556dbaf047514867c38b63e3c863
ig-zrx <omarkhalid1999824@gmail.com>
2026-03-22 13:54:29 +0200

    Primitive Sorting Algorithm (Selection Sort)
    Algorithm steps:
    - Start from the first position, and get the minimum value of all of the remaining values (including the starting position).
    - After getting the minimum value, swap it with the value in the starting position.
    - Advance the starting position to the following position.
    - Go to step (1) and repeat those operations until reaching the last value.
    Example: Sorting the list [11, 80, 22, 14, 5]
    - Initial list:
    11  80  22  14  5
    - Step 1:
    5  80  22  14  11
    - Step 2:
    5  11  22  14  80
    - Step 3:
    5  11  14  22  80
    - Final sorted list:
    5  11  14  22  80
    Task:
    Write a program to read 50 values from the user, and then sort them using this technique.

--- FileName.cpp
+++ FileName.cpp
@@ -1,134 +1,33 @@
-#include<iostream>
+#include<iostream> 
 using namespace std;
 void main()
 {
-	char R = 'R';
-	char B = 'B';
-	char board[10][10];
-	int cselection;
-	int turn = 1;
-	int r;
-	int winner = 0;
-	for (int r = 0; r < 10; r++)
+	int pos;
+	int x[50]; 
+	for (int i = 0; i < 50; i++)
 	{
-		for (int c = 0; c < 10; c++)
-		{
-			board[r][c] = ' ';
-		}
+		cin >> x[i];		
 	}
-	for (;;)
+	for (int i = 0; i < 50; i++)
 	{
-		if (turn == 1)
+		int min = 9999;
+
+		for (int j = i; j < 50; j++)
 		{
-			cout << "Player 1 Choose a Column";
-			cin >> cselection;
-			for (r = 9; r >=0 ; r--) 
+			if (x[j] < min)
 			{
-				if (board[r][cselection] == ' ')
-				{
-					board[r][cselection] = R;
-					break;
-				}
+				min = x[j];
+				pos = j;
 			}
-			for (int r = 0; r < 10; r++)
-			{
-				for (int c = 0; c < 10; c++)
-				{
-					if (board[r][c] == R)
-					{
-						if (board[r][c + 1] == R && board[r][c + 2] == R && board[r][c + 3] == R && c<=6)
-						{
-							winner = 1;
-						}
-						if (board[r + 1][c] == R && board[r + 2][c] == R && board[r + 3][c] == R && r<=6)
-						{
-							winner = 1;
-						}
-						if (board[r + 1][c + 1] == R && board[r + 2][c + 2] == R && board[r + 3][c + 3] == R && r<=6 && c<=6)
-						{
-							winner = 1;
-						}
-						if (board[r - 1][c - 1] == R && board[r - 2][c - 2] == R && board[r - 3][c - 3] == R && r>=3 && c>=3)
-						{
-							winner = 1;
-						}
-					}
-				}
-			}
-			for (int r = 0; r < 10; r++) 
-			{
-				for (int c = 0; c < 10; c++) 
-				{
-					cout << board[r][c] << " ";
-				}
-				cout << endl;
-			}
-			cout << endl;
-			if (winner == 1)
-			{
-				break;
-			}
-			turn++;
+
 		}
-		if (turn == 2)
-		{
-			cout << "Player 2 Choose a Column";
-			cin >> cselection;
-			for (int r = 9; r >= 0; r--)
-			{
-				if (board[r][cselection] == ' ')
-				{
-					board[r][cselection] = B;
-					break;
-				}
-			}
-				for (int r = 0; r < 10; r++)
-				{
-					for (int c = 0; c < 10; c++)
-					{
-						if (board[r][c] == B)
-						{
-							if (board[r][c + 1] == B && board[r][c + 2] == B && board[r][c + 3] == B && c <= 6)
-							{
-								winner = 2;
-							}
-							if (board[r + 1][c] == B && board[r + 2][c] == B && board[r + 3][c] == B && r <= 6)
-							{
-								winner = 2;
-							}
-							if (board[r + 1][c + 1] == B && board[r + 2][c + 2] == B && board[r + 3][c + 3] == B && r <= 6 && c <= 6)
-							{
-								winner = 2;
-							}
-							if (board[r - 1][c - 1] == B && board[r - 2][c - 2] == B && board[r - 3][c - 3] == B && r >= 3 && c >= 3)
-							{
-								winner = 2;
-							}
-						}
-					}
-				}
-		}
-		for (int r = 0; r < 10; r++)
-		{
-			for (int c = 0; c < 10; c++)
-			{
-				cout << board[r][c] << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;
-		if (winner == 2)
-		{
-			break;
-		}
-		turn--;
-	}
-	if (winner == 1)
-	{
-		cout << "Red Wins!";
-	}
-	if (winner == 2)
-	{
-		cout << "Blue Wins!";
+		int Z = x[i];
+		x[i] = x[pos];
+		x[pos] = Z;
+
+
 	}
 }
+
+
+
